5c67e4680a0eb0146ceb84639ff6ae32f6e6314d
Juan José Hoyos <juanjohupuj@javerianacali.edu.co>
2020-11-11 09:37:38 -0500

    sonar

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import vuelo as v
 import persona as p
 import DB as db
 import login as lg
+from flask_wtf.csrf import CSRFProtect
 
 
 infoVuelo = list()
@@ -15,6 +16,9 @@ infoPasajeros = list()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
+csrf = CSRFProtect()
+csrf.init_app(app) # Compliant
+
 db.createDB()
 
 
